9497553c06f8ecd5ab5c71039724d3e96a644049
KrishDevCrafting <krishkumer150@gmail.com>
2026-03-24 22:53:45 +0530

    fix: correct event name casing, wss.clients typo, and readyState typo

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const wss = new WebSocketServer({
   port: 8080,
 });
 
-wss.on("Connection", (socket, request) => {
+wss.on("connection", (socket, request) => {
   const ip = request.socket.remoteAddress;
+
+  socket.on("message", (rawData) => {
+    const message = rawData.toString();
+    console.log({ rawData, message, ip });
+    wss.clients.forEach((client) => {
+      if (client.readyState === 1) client.send(`Server Broadcast: ${message}`);
+    });
+  });
 });
