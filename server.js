1dd47b2818fc085dd81a91840eae3df396fda656
KrishDevCrafting <krishkumer150@gmail.com>
2026-03-24 22:36:46 +0530

    upated

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({
   port: 8080,
 });
+
+wss.on("Connection", (socket, request) => {
+  const ip = request.socket.remoteAddress;
+});
